cf66f84e20d21a99e9bf4c287f0712681a7c2a82
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2018-12-12 00:13:30 +0530

    Add files via upload

---
+++ multiple catch.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void test (int x)
+{
+  try
+  {
+    if(x==1)throw x;
+    else
+      if(x==0)throw 'x';
+    else
+      if(x==-1)throw 1.0;
+    cout<<"end of try-block \n";
+  }
+  catch(char c)
+  {
+    cout<<"Caught a character \n ";
+  }
+    catch(int m)
+    {
+      cout<<"Caught an integer \n ";
+    }
+    catch(double d)
+    {
+      cout<<"caught a double \n ";
+    }
+    cout<<"end of try-catch system \n\n";
+  }
+int main()
+{
+  cout<<"Testing multiple catches \n";
+  cout<<"x==1 \n";
+  test(1);
+  cout<<"x==0 \n";
+  test(0);
+  cout<<"x==-1 \n";
+  test(-1);
+  cout<<"x==2 \n";
+  test(2);
+  return 0;
+}
