8dd609a38fb06cc7d69e897706fd1472409a3443
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2018-12-12 00:11:06 +0530

    Add files via upload

---
+++ Excetion Handling.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"Enter the values of a and b ";
+  cin>>a>>b;
+  int x=a-b;
+  try
+  {
+    if(x!=0)
+    {
+      cout<<"result(a/x) = "<<a/x<<"\n";
+    }
+    else
+    {
+      throw(x);
+    }
+  }
+    catch(int i)
+    {
+      cout<<"exception caught:x = "<<x<<"\n";
+    }
+      cout<<"end";
+    return 0;
+}
