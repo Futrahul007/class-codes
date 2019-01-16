a0190e591652daa2e1b6f6e47134af0ed8d4d7b4
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2019-01-16 21:46:37 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class complex
+{
+	private:
+		int a,b;
+	public:
+		complex()
+		{cout<<"\n Rahul Raj";}
+};
+ int main()
+ {
+ 	complex c1,c2,c3;
+ 	return 0;
+ }
