4e691746b9f5e3278153e72ec7e936010a5449e3
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2019-01-22 11:54:39 +0530

    Add files via upload

---
+++ increment operator overloading.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class integer
+{
+	private:
+		int x;
+	public:
+		void setdata(int a)
+	   {x=a;}
+	   void showdata()
+	   {cout<<"x="<<x;}
+	   integer operator++()
+	   {
+	   	integer i;
+	   	i.x=++x;
+	   	return (i);
+	   }
+	};
+	int main()
+	{
+	integer i1,i2;
+	i1.setdata(3);
+	i1.showdata();
+	i2=++i1;
+	i1.showdata();
+	i2.showdata();
+	return 0;
+    }
